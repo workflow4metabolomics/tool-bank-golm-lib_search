cc878852894386d0a67b9f6f9ffb74cecfdbdade
gcretin <gabriel.cretin@clermont.inra.fr>
2016-05-10 16:50:47 +0200

    Add function to test Golm WS with default query

--- lib/golm_ws_api.pm
+++ lib/golm_ws_api.pm
@@ -80,6 +80,58 @@ sub connectWSlibrarySearchGolm() {
 }
 ### END of SUB
      
+     
+     
+     
+     
+=head2 METHOD test_query_golm
+
+	## Description : send a test request (default given on ws website) to golm database.
+	## Input : $self
+	## Ouput : $soap ;
+	## Usage : my $soap = connectWSlibrarySearchGolm() ;
+
+=cut
+
+sub test_query_golm() {
+	## Retrieve Values
+    my $self = shift ;
+	my $soap = SOAP::Lite
+              -> uri('http://gmd.mpimp-golm.mpg.de')
+              -> on_action( sub { join '/', 'http://gmd.mpimp-golm.mpg.de/webservices/wsLibrarySearch.asmx', $_[1] } )
+              -> proxy('http://gmd.mpimp-golm.mpg.de/webservices/wsLibrarySearch.asmx', timeout => 500);
+               
+           # Setting Content-Type myself
+           my $http_request = $soap
+              ->{'_transport'}
+              ->{'_proxy'}
+              ->{'_http_request'};
+           $http_request->content_type("text/xml; charset=utf-8");
+           
+            my $method = SOAP::Data->name('LibrarySearch')
+                ->attr({xmlns => 'http://gmd.mpimp-golm.mpg.de/webservices/wsLibrarySearch.asmx/'});
+           
+            my @params = (
+                           SOAP::Data->name('ri' => 1898),
+                           SOAP::Data->name('riWindow' => 5),
+                           SOAP::Data->name('AlkaneRetentionIndexGcColumnComposition' => 'VAR5'),
+                           SOAP::Data->name('spectrum' => "70 3 71 3 72 16 73 999 74 87 75 78 76 4 77 5 81 1 82 6 83 13 84 4 85 3 86 4 87 5 88 4 89 52 90 4 91 2 97 2 98 1 99 4 100 12 101 16 102 9 103 116 104 11 105 26 106 2 107 1 111 1 112 1 113 4 114 11 115 7 116 5 117 93 118 9 119 8 126 1 127 3 128 3 129 101 130 19 131 25 132 4 133 60 134 8 135 4 140 1 141 1 142 4 143 13 144 2 145 6 146 1 147 276 148 44 149 27 150 3 151 1 156 1 157 70 158 12 159 5 160 148 161 26 162 7 163 8 164 1 168 1 169 2 170 1 172 3 173 4 174 1 175 4 177 4 186 2 187 1 189 28 190 7 191 13 192 2 193 1 201 5 202 1 203 3 204 23 205 162 206 31 207 16 208 2 210 2 214 1 215 2 216 8 217 88 218 18 219 8 220 1 221 6 222 1 229 23 230 6 231 11 232 3 233 4 234 3 235 1 243 1 244 2 245 1 246 2 247 1 256 1 262 3 263 1 269 2 270 1 274 4 275 1 277 4 278 1 291 7 292 2 293 1 300 1 305 4 306 1 307 4 308 1 318 1 319 122 320 37 321 17 322 3 323 1 343 1 364 2 365 1" ) );
+			
+			my $som = $soap->call($method => @params);
+			die $som->faultstring if ($som->fault);
+			
+			## Get the hits + status of the query
+			my $results = $som->result->{Results} ;
+			my $status = $som->result->{Status} ;
+			
+			if ($status eq 'success' && $results ne '') {
+				print "\n\n\nThe test request succeeded.\n\n\n" ;
+			}
+			else { croak "\n\n\nSomething went wrong with the test request. Status delivered by Golm = ".$status."\n\n" ; }
+}
+### END of SUB
+     
+     
 
 =head2 METHOD LibrarySearch
 
