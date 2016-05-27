ba6700e950386c9d96a707d7383074a0100150ce
gcretin <gabriel.cretin@clermont.inra.fr>
2016-05-27 16:27:13 +0200

    create output directory

--- golm_ws_lib_search.pl
+++ golm_ws_lib_search.pl
@@ -189,6 +189,8 @@ foreach my $spectrum (@$encoded_spectra){
 
 ############# -------------- Build outputs -------------- ############# :
 
+if( ! -d "output/" ) { system "mkdir output" ; }
+	
 my $jsons_obj = $o_output->build_json_res_object(\@hits) ;
 $o_output->write_json_skel(\$json_file, $jsons_obj) ;
 
